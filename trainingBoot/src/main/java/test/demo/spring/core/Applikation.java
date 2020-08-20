efc85a339831cf75c36ce69e88f274e176a7dc0d
Robert Błach <robertbl1@wp.pl>
2020-08-20 22:04:07 +0200

    pierwsza konfiguracja springa

---
+++ trainingBoot/src/main/java/test/demo/spring/core/Applikation.java
@@ -0,0 +1,13 @@
+package test.demo.spring.core;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Applikation {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Applikation.class, args);
+    }
+}
---
+++ trainingBoot/src/main/java/test/demo/spring/core/controller/TestController.java
@@ -0,0 +1,16 @@
+package test.demo.spring.core.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class TestController {
+
+    @GetMapping
+    public String testEndpoint(){
+        return "test spring boot";
+    }
+}
